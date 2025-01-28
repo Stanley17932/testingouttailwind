86a7888839e587e5318d4b7975a60c63c05d34ce
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2025-01-28 14:36:26 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+const App = () => {
+ 
+
+  return (
+     <div> 
+      <section className ="m-12 flex h-screen items-center justify-center bg-twitter-blue 
+      text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+          purus ut nunc ultricies aliquam. Nullam nec purus ut nunc ultricies
+          aliquam. Nullam nec purus ut nunc ultricies aliquam. Nullam nec purus
+          ut nunc ultricies aliquam. Nullam nec purus ut nunc ultricies aliquam.
+          Nullam nec purus ut nunc ultricies aliquam. Nullam nec purus ut nunc
+          ultricies aliquam. Nullam nec purus ut nunc ultricies aliquam. Nullam
+        </p>
+
+      </section>
+      </div>
+  )
+}
+
+export default App
